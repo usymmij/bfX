0a05f90fb6674cc399b43c27be69a6291ef70c30
usymmij <usymmij@gmail.com>
2025-07-28 00:34:59 -0400

    4 bit adder

--- src/bfx.v
+++ src/bfx.v
@@ -1,43 +1,76 @@
 `include <memory.v>
 `include <decode.v>
 
-module bfX(input clk, [7:0] in, output [7:0] out);
+module bfX (
+    input clk,
+    input [7:0] in,
+    output [7:0] out
+);
 
-    reg [15:0] pc;
+  reg [15:0] pc;
 
-    reg memoryActive;
-    reg memoryRSel;
-    wire [15:0] memoryAddr;
-    wire [7:0] memoryWrite;
-    wire [7:0] memoryRead;
+  reg memoryActive;
+  reg memoryRSel;
+  wire [15:0] memoryAddr;
+  wire [7:0] memoryWrite;
+  wire [7:0] memoryRead;
 
-    assign memoryAddr = pc;
+  assign memoryAddr   = pc;
+  assign memoryActive = 1;
+  assign memoryRSel   = 1;
 
-    mem memory (clk, memoryActive, memoryRSel,
-                memoryAddr, memoryWrite, memoryRead);
+  mem memory (
+      clk,
+      memoryActive,
+      memoryRSel,
+      memoryAddr,
+      memoryWrite,
+      memoryRead
+  );
 
-    always @(posedge clk) begin
-        pc = pc + 1;
-    end
+  always @(posedge clk) begin
+    pc = pc + 1;
+  end
 
-    wire dataCounter, data, io, branch, stop, mode;
+  wire dataCounter, data, io, branch, stop, mode;
 
-    decode ix_decode (clk, memoryRead, 
-            dataCounter, data, io, branch, stop, mode);
+  decode ix_decode (
+      clk,
+      memoryRead,
+      dataCounter,
+      data,
+      io,
+      branch,
+      stop,
+      mode
+  );
+
+  reg [15:0] dc;
+
+  assign out = {2'h0, dataCounter, data, io, branch, stop, mode};
+
+  initial begin
+    pc = 16'h0;
+    dc = 16'h256;
+  end
 
 endmodule
 
 module tb_bfx_memory ();
 
-    reg clk;
-    reg [7:0] in;
-    wire [7:0] out;
-    bfX bfx (clk, in, out);
+  reg clk;
+  reg [7:0] in;
+  wire [7:0] out;
+  bfX bfx (
+      clk,
+      in,
+      out
+  );
 
-    always #5 clk = ~clk;
+  always #5 clk = ~clk;
 
-    initial begin 
-        clk = 0;
-    end
+  initial begin
+    clk = 0;
+  end
 
 endmodule
--- src/decode.v
+++ src/decode.v
@@ -1,48 +1,52 @@
 module decode (
     input clk,
     input [7:0] ix,
-    output dataCounter, data, io, branch, stop, mode
+    output dataCounter,
+    data,
+    io,
+    branch,
+    stop,
+    mode
 );
-    assign mode = ix[0];
-    assign dataCounter = ~(ix[3] | ix[2] | ix[1]);
-    assign data = ~(ix[3] | ix[2]) & ix[1];
-    assign io = ~(ix[3] | ix[1]) & ix[2];
-    assign branch= ~ix[3] & ix[2] & ix[1];
-    assign stop = ix[3] &~(ix[2] | ix[1] | ix[0]) ;
+  assign dataCounter = ~(ix[3] | ix[2] | ix[1]);
+  assign data = ~(ix[3] | ix[2]) & ix[1];
+  assign io = ~(ix[3] | ix[1]) & ix[2];
+  assign branch = ~ix[3] & ix[2] & ix[1];
+  assign stop = ix[3] & ~(ix[2] | ix[1] | ix[0]);
+  assign mode = ix[0];
 
 endmodule
 
-module tb_decode (
-);
+module tb_decode ();
 
-    reg clk;
-    reg [7:0] ix;
-    wire dataCounter, data, io, branch, stop, mode;
+  reg clk;
+  reg [7:0] ix;
+  wire dataCounter, data, io, branch, stop, mode;
 
-    decode test (clk, ix, dataCounter, data, io, branch, stop, mode);
+  decode test (
+      clk,
+      ix,
+      dataCounter,
+      data,
+      io,
+      branch,
+      stop,
+      mode
+  );
 
-    always #5 clk = ~clk;
+  always #5 clk = ~clk;
 
-    initial begin
-        clk = 1'b0;
-        #4
-        ix = 8'h0;
-        #10
-        ix = 8'h1;
-        #10
-        ix = 8'h2;
-        #10
-        ix = 8'h3;
-        #10
-        ix = 8'h4;
-        #10
-        ix = 8'h5;
-        #10
-        ix = 8'h6;
-        #10
-        ix = 8'h7;
-        #10
-        ix = 8'h8;
-    end
+  initial begin
+    clk = 1'b0;
+    #4 ix = 8'h0;
+    #10 ix = 8'h1;
+    #10 ix = 8'h2;
+    #10 ix = 8'h3;
+    #10 ix = 8'h4;
+    #10 ix = 8'h5;
+    #10 ix = 8'h6;
+    #10 ix = 8'h7;
+    #10 ix = 8'h8;
+  end
 
 endmodule
